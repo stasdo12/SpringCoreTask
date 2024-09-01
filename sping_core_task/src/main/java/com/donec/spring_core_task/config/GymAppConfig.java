2dd7beb664fc68a35ffdc99df2de39d1de682ffb
Stanislav Donetc <stanislavdonetc@gmail.com>
2024-09-01 12:04:56 +0300

    resolve Punctuation error

---
+++ sping_core_task/src/main/java/com/donec/spring_core_task/config/GymAppConfig.java
@@ -0,0 +1,12 @@
+package com.donec.spring_core_task.congig;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan(basePackages = "com.donec.spring_core_task")
+@PropertySource("classpath:application.properties")
+public class GymAppConfig {
+}
--- sping_core_task/src/main/java/com/donec/spring_core_task/congig/GymAppConfig.java
+++
@@ -1,2 +0,0 @@
-package com.donec.spring_core_task.congig;public class GymAppConfig {
-}
